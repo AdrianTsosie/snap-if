afb6e93481e7793b9a27e5f5a3b1da4372575b31
Adian Tsosie <addve12@yahoo.com>
2019-01-16 09:11:51 -0700

    My Snap If assignment

---
+++ snap-if.js
@@ -0,0 +1,33 @@
+/**
+ * Function that checks if you know the meaning of life.
+ *
+ * @parm question potential meaning of life.
+ * @return 42 is the answer to life if question === 42
+ */
+	function answerToLife(question) {
+		if(question === 42) {
+			return "42 is the answer to life"
+		} else {
+			return "false try again"
+		}
+}
+
+console.log(answerToLife(42))
+
+
+
+/**
+{
+	let argumentProvided = 42;
+	let 42 = 42;
+
+	function myFunction(argumentProvided, 42) {
+		let
+		return argumentProvided = 42; // Function returns "42 is the answer to life"
+	}
+}
+
+myFunction(42, 42);
+*/
+
+
